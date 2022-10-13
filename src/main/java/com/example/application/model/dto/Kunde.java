82233298c25a11bae6aab3fbe12ba21fd1c7f4ed
Mijusa <106547857+Mijusa@users.noreply.github.com>
2022-10-13 18:45:22 +0200

    Kunden klasse

---
+++ src/main/java/com/example/application/model/dto/Kunde.java
@@ -0,0 +1,11 @@
+package com.example.application.model.dto;
+
+import lombok.Data;
+
+@Data
+public class Kunde {
+    private String vorname;
+    private String nachname;
+    private Integer id;
+    private String adresse;
+}
