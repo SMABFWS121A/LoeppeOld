df059b3220f727fb2af2abb05211e67e9bec9a36
Edward <loewen.edward1521@gmail.com>
2022-10-15 20:42:39 +0200

    Class artikel

--- src/main/java/com/example/application/model/dto/Artikel.java
+++ src/main/java/com/example/application/model/dto/Artikel.java
@@ -1,5 +1,8 @@
 package com.example.application.model.dto;
 
 public class Artikel {
-    
+    private Integer id;
+    private String name;
+    private String description;
+    private Double price;
 }
